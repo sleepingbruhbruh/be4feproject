1dfdfe9b70fa51646e66080a692973507faba67b
Korn <korn.sd15@gmail.com>
2026-03-24 00:18:13 +0700

    added startdate and enddate

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,191 +1,221 @@
 const Booking = require('../models/Booking');
 const Hotel = require('../models/Hotel');
+
 //@desc     Get all bookings
 //@route    GET /api/v1/bookings
 //@access   Public
-exports.getBookings=async (req,res,next)=>{
+exports.getBookings = async (req, res, next) => {
     let query;
-    //General users can see only their bookings!
-    if(req.user.role !== 'admin'){ //General users can see only their bookings!
-        query=Booking.find({user:req.user.id}).populate({
-            path:'hotel',
-            select:'name address tel'
+    // General users can see only their bookings!
+    if (req.user.role !== 'admin') {
+        query = Booking.find({ user: req.user.id }).populate({
+            path: 'hotel',
+            select: 'name address tel'
         });
-    }else{ //If you are an admin, you can see all bookings!
+    } else {
+        // If you are an admin, you can see all bookings!
         if (req.params.hotelId) {
             console.log(req.params.hotelId);
             query = Booking.find({ hotel: req.params.hotelId }).populate({
-                path:'hotel',
-                select:'name address tel'
+                path: 'hotel',
+                select: 'name address tel'
             });
         } else {
-            query=Booking.find().populate({
-                path:'hotel',
-                select:'name address tel'
+            query = Booking.find().populate({
+                path: 'hotel',
+                select: 'name address tel'
             });
         }
     }
 
     try {
-        const bookings= await query;
+        const bookings = await query;
 
         res.status(200).json({
-            success:true,
-            count:bookings.length,
+            success: true,
+            count: bookings.length,
             data: bookings
         });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success:false,message:
-        "Cannot find Booking"});
+        return res.status(500).json({ success: false, message: "Cannot find Booking" });
     }
-
 };
 
 exports.getBooking = async (req, res, next) => {
-  try {
-    const booking = await Booking.findById(req.params.id).populate({
-      path: 'hotel',
-      select: 'name address tel'
-    });
+    try {
+        const booking = await Booking.findById(req.params.id).populate({
+            path: 'hotel',
+            select: 'name address tel'
+        });
 
-    if (!booking) {
-      return res.status(404).json({
-        success: false,
-        message: `No booking with the id of ${req.params.id}`
-      });
+        if (!booking) {
+            return res.status(404).json({
+                success: false,
+                message: `No booking with the id of ${req.params.id}`
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: booking
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Cannot find Booking"
+        });
     }
-
-    res.status(200).json({
-      success: true,
-      data: booking
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      message: "Cannot find Booking"
-    });
-  }
 };
+
 //@desc    Add booking
 //@route   POST /api/v1/hotels/:hotelId/bookings
 //@access  Private
 exports.addBooking = async (req, res, next) => {
-  try {
-    req.body.hotel = req.params.hotelId;
+    try {
+        req.body.hotel = req.params.hotelId;
 
-    const hotel = await Hotel.findById(req.params.hotelId);
+        const hotel = await Hotel.findById(req.params.hotelId);
 
-    if (!hotel) {
-      return res.status(404).json({
-        success: false,
-        message: `No hotel with the id of ${req.params.hotelId}`
-      });
+        if (!hotel) {
+            return res.status(404).json({
+                success: false,
+                message: `No hotel with the id of ${req.params.hotelId}`
+            });
+        }
+
+        // Validate startDate and endDate are present
+        const { startDate, endDate } = req.body;
+
+        if (!startDate || !endDate) {
+            return res.status(400).json({
+                success: false,
+                message: 'Please provide both startDate and endDate'
+            });
+        }
+
+        // Validate endDate is the same as or after startDate
+        if (new Date(endDate) < new Date(startDate)) {
+            return res.status(400).json({
+                success: false,
+                message: 'End date must be the same as or after start date'
+            });
+        }
+
+        // Add user Id to req.body
+        req.body.user = req.user.id;
+
+        // Check for existed bookings
+        const existedBookings = await Booking.find({ user: req.user.id });
+
+        // If the user is not an admin, they can only create 3 bookings.
+        if (existedBookings.length >= 3 && req.user.role !== 'admin') {
+            return res.status(400).json({
+                success: false,
+                message: `The user with ID ${req.user.id} has already made 3 bookings`
+            });
+        }
+
+        const booking = await Booking.create(req.body);
+
+        res.status(200).json({
+            success: true,
+            data: booking
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Cannot create Booking"
+        });
     }
-
-    // add user Id to req.body
-    req.body.user = req.user.id;
-
-    // Check for existed booking
-    const existedBookings = await Booking.find({ user: req.user.id });
-
-    // If the user is not an admin, they can only create 3 booking.
-    if (existedBookings.length >= 3 && req.user.role !== 'admin') {
-      return res.status(400).json({
-        success: false,
-        message: `The user with ID ${req.user.id} has already made 3 bookings`
-      });
-    }
-
-    const booking = await Booking.create(req.body);
-
-    res.status(200).json({
-      success: true,
-      data: booking
-    });
-
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      message: "Cannot create Booking"
-    });
-  }
 };
 
 exports.updateBooking = async (req, res, next) => {
-  try {
-    let booking = await Booking.findById(req.params.id);
+    try {
+        let booking = await Booking.findById(req.params.id);
 
-    if (!booking) {
-      return res.status(404).json({
-        success: false,
-        message: `No booking with the id of ${req.params.id}`
-      });
-    }
+        if (!booking) {
+            return res.status(404).json({
+                success: false,
+                message: `No booking with the id of ${req.params.id}`
+            });
+        }
 
-    if (booking.user.toString() !== req.user.id && req.user.role !== 'admin') {
-        return res.status(401).json({
-          success: false,
-          message: `User ${req.user.id} is not authorized to update this booking`
+        if (booking.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({
+                success: false,
+                message: `User ${req.user.id} is not authorized to update this booking`
+            });
+        }
+
+        // If either date is being updated, re-validate the range
+        const startDate = req.body.startDate ? new Date(req.body.startDate) : booking.startDate;
+        const endDate = req.body.endDate ? new Date(req.body.endDate) : booking.endDate;
+
+        if (endDate < startDate) {
+            return res.status(400).json({
+                success: false,
+                message: 'End date must be the same as or after start date'
+            });
+        }
+
+        booking = await Booking.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        res.status(200).json({
+            success: true,
+            data: booking
         });
-      }
 
-    booking = await Booking.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true
-      }
-    );
-
-    res.status(200).json({
-      success: true,
-      data: booking
-    });
-
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      message: "Cannot update Booking"
-    });
-  }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Cannot update Booking"
+        });
+    }
 };
 
 exports.deleteBooking = async (req, res, next) => {
-  try {
-    const booking = await Booking.findById(req.params.id);
+    try {
+        const booking = await Booking.findById(req.params.id);
 
-    if (!booking) {
-      return res.status(404).json({
-        success: false,
-        message: `No booking with the id of ${req.params.id}`
-      });
+        if (!booking) {
+            return res.status(404).json({
+                success: false,
+                message: `No booking with the id of ${req.params.id}`
+            });
+        }
+
+        // Make sure user is the booking owner
+        if (booking.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({
+                success: false,
+                message: `User ${req.user.id} is not authorized to delete this booking`
+            });
+        }
+
+        await booking.deleteOne();
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Cannot delete Booking"
+        });
     }
-
-    // Make sure user is the booking owner
-    if (booking.user.toString() !== req.user.id && req.user.role !== 'admin') {
-      return res.status(401).json({
-        success: false,
-        message: `User ${req.user.id} is not authorized to delete this booking`
-      });
-    }
-
-    await booking.deleteOne();
-
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      message: "Cannot delete Booking"
-    });
-  }
 };
--- models/Booking.js
+++ models/Booking.js
@@ -1,24 +1,35 @@
 const mongoose = require('mongoose');
-
-const BookingSchema=new mongoose.Schema({
-    bookDate: {
+ 
+const BookingSchema = new mongoose.Schema({
+    startDate: {
         type: Date,
-        required:true
+        required: true
     },
-    user:{
-        type:mongoose.Schema.ObjectId,
+    endDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value >= this.startDate;
+            },
+            message: 'End date must be the same as or after start date'
+        }
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
         ref: 'User',
-        required:true
+        required: true
     },
-    hotel:{
-        type:mongoose.Schema.ObjectId,
+    hotel: {
+        type: mongoose.Schema.ObjectId,
         ref: 'Hotel',
-        required:true
+        required: true
     },
     createdAt: {
         type: Date,
         default: Date.now
     }
 });
-
-module.exports=mongoose.model('Booking',BookingSchema);
+ 
+module.exports = mongoose.model('Booking', BookingSchema);
+ 
