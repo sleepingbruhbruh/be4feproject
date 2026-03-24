e627dd5be3a8ddd4bb56e79828b6de24382cc794
Korn <korn.sd15@gmail.com>
2026-03-24 08:49:55 +0700

    Revert "fixed logic error"
    This reverts commit fd8a02456dc4dd3a8959bbfc545a4f08cf3a8056.

--- models/Booking.js
+++ models/Booking.js
@@ -1,41 +1,34 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
+ 
 const BookingSchema = new mongoose.Schema({
-  startDate: {
-    type: Date,
-    required: true,
-  },
-  endDate: {
-    type: Date,
-    validate: {
-      validator: function (value) {
-        let startDate;
-        if (typeof this.getUpdate === "function") {
-          const update = this.getUpdate();
-          startDate = update.startDate ?? update["$set"]?.startDate;
-        } else {
-          startDate = this.startDate;
-        }
-        if (!startDate) return true;
-        return value >= new Date(startDate);
-      },
-      message: "End date must be the same as or after start date",
+    startDate: {
+        type: Date,
+        required: true
     },
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  hotel: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Hotel",
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    endDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return value >= this.startDate;
+            },
+            message: 'End date must be the same as or after start date'
+        }
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    hotel: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Hotel',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
-
-module.exports = mongoose.model("Booking", BookingSchema);
+ 
+module.exports = mongoose.model('Booking', BookingSchema);
